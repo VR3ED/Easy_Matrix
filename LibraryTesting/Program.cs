2e117103d70d4bfd979de3072ff0c49c8506c119
Francesco Cavallini <videofrancesco11@gmail.com>
2024-09-05 22:57:01 +0200

    modifica al main per poter runnare tutte le matrici

--- LibraryTesting/Program.cs
+++ LibraryTesting/Program.cs
@@ -8,70 +8,100 @@ int maxIter = 50000;
 
 string file_path = "";
 
-if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+for (int matrixIndex = 0; matrixIndex < 4; matrixIndex++)
 {
-    file_path = "/home/Cava/Documents/Repos/C#/Easy_Matrix/EasyMatrix/Matrixes/spa1.mtx";
-}
-else
-{
-    file_path = @"C:\Users\Cava\Documents\REPOS\C#\Library Metodi del Calcolo Scientifico\EasyMatrix\Matrixes\spa1.mtx";
-}
-
-AccurateMatrix A = new AccurateMatrix(file_path);
-
-//Console.WriteLine(A.IsSymmetricPositiveDefinite());
-//Console.ReadKey();
-//Console.Clear();
-
-decimal[] b = new decimal[A.columns]; // inizializzazione vettore dei termini noti
-b = Enumerable.Repeat(1.0m, A.columns).ToArray(); // inserimento di soli 1 all'interno del vettore di termini noti
-
-
-//foreach tollerance required
-for(int i=0; i<tol.Length-1; i++)
-{
-    //foreach solver implemented
-    for(int j=0; j<4; j++)
+    
+    string matrixName = "";
+    
+    //define wich matrix to use
+    switch (matrixIndex)
     {
-        decimal[] result = [];
-        IterativeSolver solver;
-        switch (j)
+        case 0:
         {
-            case 0:
-            {
-                solver = new JacobiSolver(A, b, tol[i], maxIter);
-                break;
-            }
-            case 1:
-            {
-                solver = new GaussSeidelSolver(A, b, tol[i], maxIter);
-                break;
-            }
-            case 2:
-            {
-                solver = new GradientSolver(A, b, tol[i], maxIter);
-                break;
-            }
-            case 3:
-            {
-                solver = new ConjugateGradientSolver(A, b, tol[i], maxIter);
-                break;
-            }
-            default:
-            {
-                solver = new ConjugateGradientSolver(A, b, tol[4], maxIter);
-                break;
-            }
+            matrixName = "spa1.mtx";
+            break;
         }
-        result = solver.Solve();
-
-        Console.WriteLine("Solution:");
-        foreach (var item in result)
+        case 1:
         {
-            Console.WriteLine(item);
+            matrixName = "spa2.mtx";
+            break;
+        }
+        case 2:
+        {
+            matrixName = "vem1.mtx";
+            break;
+        }
+        case 3:
+        {
+            matrixName = "vem2.mtx";
+            break;
+        }
+        default:
+        {
+            matrixName = "spa3.mtx";
+            break;
         }
     }
-} 
+    
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+    {
+        file_path = "/home/Cava/Documents/Repos/C#/Easy_Matrix/EasyMatrix/Matrixes/"+matrixName;
+    }
+    else
+    {
+        file_path = @"C:\Users\Cava\Documents\REPOS\C#\Library Metodi del Calcolo Scientifico\EasyMatrix\Matrixes\"+matrixName;
+    }
+
+    AccurateMatrix A = new AccurateMatrix(file_path);
+
+    decimal[] b = new decimal[A.columns]; // inizializzazione vettore dei termini noti
+    b = Enumerable.Repeat(1.0m, A.columns).ToArray(); // inserimento di soli 1 all'interno del vettore di termini noti
+
+
+    //foreach tollerance required
+    for(int i=0; i<tol.Length-1; i++)
+    {
+        //foreach solver implemented
+        for(int j=0; j<4; j++)
+        {
+            decimal[] result = [];
+            IterativeSolver solver;
+            //define wich solver to use
+            switch (j)
+            {
+                case 0:
+                {
+                    solver = new JacobiSolver(A, b, tol[i], maxIter);
+                    break;
+                }
+                case 1:
+                {
+                    solver = new GaussSeidelSolver(A, b, tol[i], maxIter);
+                    break;
+                }
+                case 2:
+                {
+                    solver = new GradientSolver(A, b, tol[i], maxIter);
+                    break;
+                }
+                case 3:
+                {
+                    solver = new ConjugateGradientSolver(A, b, tol[i], maxIter);
+                    break;
+                }
+                default:
+                {
+                    solver = new ConjugateGradientSolver(A, b, tol[4], maxIter);
+                    break;
+                }
+            }
+            result = solver.Solve();
+        
+            Console.WriteLine("Finished "+ A.matrix_name +"(simmetrica e definita positiva: "+ A.IsSymmetricPositiveDefinite() + ")" + " " + solver.GetType().Name);
+        
+        }
+    }
+}
 
 Console.ReadKey();
 
